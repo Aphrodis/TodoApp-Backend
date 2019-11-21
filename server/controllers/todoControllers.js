e51b9023e39ae40385922dbfcb773e6419c01955
Aphrodis <izabayoaphrodis@gmail.com>
2019-11-21 22:02:07 +0200

    Refactor codes to increase maintainability

--- server/controllers/todoControllers.js
+++ server/controllers/todoControllers.js
@@ -10,10 +10,7 @@ const createTask = async (req, res) => {
         const task = req.body;
         const result1 = Schema.validateTodo(task);
         if (result1.error) {
-            return res.status(400).json({
-                status: 400,
-                message: result1.error.details[0].message,
-            });
+            return res.status(400).json({ status: 400, message: result1.error.details[0].message, });
         }
         const taskid = uuid();
         const createdon = new Date();
@@ -28,10 +25,7 @@ const createTask = async (req, res) => {
             data: newTask.rows[0],
         });
     } catch (err) {
-        return res.status(500).json({
-            status: 500,
-            message: err.message,
-        });
+        return res.status(500).json({ status: 500, message: err.message, });
     }
 };
 const getAllTodos = async (req, res) => {
@@ -41,39 +35,21 @@ const getAllTodos = async (req, res) => {
         const allTodos = await pool.query(selectTodos, [userid]);
 
         const pageNo = req.query.pageNo ? parseInt(req.query.pageNo) : 1;
-
         const pageSize = req.query.pageSize ? parseInt(req.query.pageSize) : 10;
         const startIndex = (pageNo - 1) * pageSize;
         const endIndex = pageSize * pageNo;
-
         const totalTodos = allTodos.rows.length;
         const totalPages = Math.ceil(totalTodos / pageSize);
         let retrievedTodos = allTodos.rows.slice(startIndex, endIndex);
-
         const itemsOnPage = retrievedTodos.length;
 
         if (allTodos.rows.length === 0) {
-            return res.status(404).json({
-                status: 404,
-                message: 'You have no todos',
-            });
+            return res.status(404).json({ status: 404, message: 'You have no todos', });
         } else {
-            return res.status(200).json({
-                status: 200,
-                message: 'Todos retrieved successfully',
-                totalTodos,
-                totalPages,
-                itemsOnPage,
-                pageNo,
-                retrievedTodos,
-            });
+            return res.status(200).json({ status: 200, message: 'Todos retrieved successfully', totalTodos, totalPages, itemsOnPage, pageNo, retrievedTodos, });
         }
-
     } catch (err) {
-        return res.status(500).json({
-            status: 500,
-            message: err.message,
-        })
+        return res.status(500).json({ status: 500, message: err.message, })
     }
 };
 
@@ -108,49 +84,26 @@ const updateTask = async (req, res) => {
     try {
         const { userid } = req.userExists;
         const { taskid } = req.params;
-
         const getTaskQuery = 'SELECT * FROM todos WHERE userid=$1 AND taskid=$2';
 
-        const selectValues = [
-            userid,
-            taskid
-        ];
+        const selectValues = [userid, taskid];
         const getTaskToBeUpdated = await pool.query(getTaskQuery, selectValues);
 
         if (!getTaskToBeUpdated.rows[0]) {
-            return res.status(404).json({
-                status: 404,
-                message: 'Task to be updated was not found',
-            });
+            return res.status(404).json({ status: 404, message: 'Task to be updated was not found', });
         }
         const result2 = await Schema.validateTodo(req.body);
         if (result2.error) {
-            return res.status(401).json({
-                status: 401,
-                message: result2.error.details[0].message,
-            })
+            return res.status(401).json({ status: 401, message: result2.error.details[0].message, });
         }
-        const updateQuery = `UPDATE todos
-                SET title=$1,description=$2 WHERE userid=$3 AND taskid=$4 RETURNING *`;
+        const updateQuery = `UPDATE todos SET title=$1,description=$2 WHERE userid=$3 AND taskid=$4 RETURNING *`;
 
-        const values = [
-            req.body.title || getTaskToBeUpdated.rows[0].title,
-            req.body.description || getTaskToBeUpdated.rows[0].description,
-            getTaskToBeUpdated.rows[0].userid,
-            getTaskToBeUpdated.rows[0].taskid
-        ];
+        const values = [req.body.title || getTaskToBeUpdated.rows[0].title, req.body.description || getTaskToBeUpdated.rows[0].description, getTaskToBeUpdated.rows[0].userid, getTaskToBeUpdated.rows[0].taskid];
         const updatedTask = await pool.query(updateQuery, values);
 
-        return res.status(200).json({
-            status: 200,
-            message: 'Task updated successfully',
-            data: updatedTask.rows[0],
-        })
+        return res.status(200).json({ status: 200, message: 'Task updated successfully', data: updatedTask.rows[0], });
     } catch (err) {
-        return res.status(500).json({
-            status: 500,
-            message: err.message,
-        });
+        return res.status(500).json({ status: 500, message: err.message, });
     }
 };
 const deleteTask = async (req, res) => {
